27cd7972850818a5edb6588cf32dc84c5fa5ccee
swautomatetesting <sw.automate.testing@gmail.com>
2021-03-12 22:43:13 +0000

    initial commit

---
+++ src/test/java/com/stepsdefs/LoginStepDef.java
@@ -0,0 +1,12 @@
+package com.stepsdefs;
+
+import org.junit.Test;
+
+public class LoginStepDef {
+	@Test
+	public void withValid()
+	{
+		System.out.print("heloooo");
+	}
+
+}
---
+++ src/test/resources/features/search.feature
@@ -0,0 +1,5 @@
+@search @TU-123 @sprint-14
+Feature: Search
+@searchexamples1
+Scenario: Verify Search with valid product name
+Given I am on Home page
